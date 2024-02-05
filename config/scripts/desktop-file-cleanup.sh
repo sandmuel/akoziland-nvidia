3406961e96ef260a0dcbe76071e098c276dcbf63
sandmuel <samuel@sandmuel.net>
2024-02-05 06:12:13 +0000

    fix for my mistake (brought back stuff)

---
+++ config/scripts/desktop-file-cleanup.sh
@@ -0,0 +1,4 @@
+rm /usr/share/applications/rofi.desktop
+rm /usr/share/applications/rofi-theme-selector.desktop
+rm /usr/share/applications/fish.desktop
+rm /usr/share/applications/Alacritty.desktop
